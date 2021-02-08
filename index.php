93165c4175caeb32da3cca9da06c4fce0037faae
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-02-08 23:05:49 +0700

    Commit

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-    echo "Hello";
+    require('vendor/autoload.php');
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3ac56dcea2061aea040d8f284b5a7048f6921b2b',
+    'name' => 'vendor/monolog',
+  ),
+  'versions' => 
+  array (
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'abc80e0db8ad31c03b373977fc997e980800f9c2',
+    ),
+    'vendor/monolog' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ac56dcea2061aea040d8f284b5a7048f6921b2b',
+    ),
+  ),
+);
