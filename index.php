9a35f2f20dd786cb31c452610495043949b3292f
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-03-07 16:14:52 +0700

    K0mmjt

--- index.php
+++ index.php
@@ -8,5 +8,14 @@
         $db["host"], $db["port"], $db["user"], $db["pass"],
         ltrim($db["path"], "/")
     ));
-    
+    echo "Importing CSV data..." . PHP_EOL;
+    $match = ["week", "month", "quarter"];
+    for ($i = 0; $i < 3; $i++) {
+        if (($file = fopen("csv_data/".$match[$i]."csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($file, 0, "|")) !== FALSE) {
+                $size = count($data);
+                echo $size;
+            }
+        } else echo "Error opening file";
+    }
 ?>
