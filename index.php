d4c5571b15d49da1f9ef0c23399067f120cfe664
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-03-08 14:02:24 +0700

    K0mmjt

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
     ));
     echo "Importing CSV data..." . PHP_EOL;
     $match = ["week", "month", "quarter"];
-    for ($i = 0; $i < 3; $i++) {
+    foreach ($match as $match_code) {
         if (($file = fopen("csv_data/".$match[$i].".csv", "r")) !== FALSE) {
-            while (($data = fgetcsv($file, 0, "|")) !== FALSE) {
+            /*while (($data = fgetcsv($file, 0, "|")) !== FALSE) {
                 $size = count($data);
                 foreach ($data as &$temp) {
                     if ($temp === "") $temp = '0';
@@ -25,10 +25,25 @@
                 } catch (Exception $e) {
                     $ok = false;
                     echo $e . PHP_EOL;
-                    //exit;
+                    exit;
                 }
                 if ($ok) echo "<br/>Added!<br/>";
                 //for ($i = 0; $i < $size; $i++) echo "<br />".$data[$i];
+            }*/
+            $ok = true;
+            try {
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if (($pdo->query("COPY $match_code 
+                                        FROM 'csv_data/$match_code.csv' 
+                                        DELIMITER '|' 
+                                        CSV HEADER;")) === FALSE) {
+                    echo "Error";
+                    exit;
+                }
+            } catch (Exception $e) {
+                $ok = false;
+                echo $e . PHP_EOL;
+                exit;
             }
             fclose($file);
         } else echo "Error opening file";
