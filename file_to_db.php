3b31fc2e87fd9b31d068845c3737945d68df6c79
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-03-11 10:42:50 +0700

    Commit

--- file_to_db.php
+++ file_to_db.php
@@ -1,27 +1,31 @@
 <?php
     require('vendor/autoload.php');
-    // include "index/index.php";
     $db = parse_url(getenv("DATABASE_URL"));
     $pdo = new PDO("pgsql:" . sprintf(
         "host=%s;port=%s;user=%s;password=%s;dbname=%s",
         $db["host"], $db["port"], $db["user"], $db["pass"],
         ltrim($db["path"], "/")
     ));
+    echo "Importing CSV data..." . PHP_EOL;
     $match = ["week", "month", "quarter"];
     foreach ($match as $match_code) {
         if (($file = fopen("csv_data/".$match_code.".csv", "r")) !== FALSE) {
             while (($data = fgetcsv($file, 0, "|")) !== FALSE) {
+                $size = count($data);
+                foreach ($data as &$temp) {
+                    if ($temp === "") $temp = '0';
+                }
                 $ok = true;
                 try {
                     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $add_row = $pdo->prepare("UPDATE $match_code SET Notice = '' WHERE Notice = '0'");
-                    $add_row->execute();
+                    $add_row = $pdo->prepare("INSERT INTO $match_code VALUES (DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                    $add_row->execute((array)$data);
                 } catch (Exception $e) {
                     $ok = false;
                     echo $e . PHP_EOL;
                     exit;
                 }
-                if ($ok) echo "<br/>Modified!<br/>";
+                if ($ok) echo "<br/>Added!<br/>";
             }
             fclose($file);
             echo "<br /> OK";
