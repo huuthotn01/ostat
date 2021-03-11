cda92fef628d9cb98a896d82afeb2754d198cdc6
huuthotn01 <tho.trannguyenhuu@hcmut.edu.vn>
2021-03-11 10:45:49 +0700

    Commit

--- index.php
+++ index.php
@@ -9,22 +9,16 @@
     ));
     $match = ["week", "month", "quarter"];
     foreach ($match as $match_code) {
-        if (($file = fopen("csv_data/".$match_code.".csv", "r")) !== FALSE) {
-            while (($data = fgetcsv($file, 0, "|")) !== FALSE) {
-                $ok = true;
-                try {
-                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $add_row = $pdo->prepare("UPDATE $match_code SET Notice = '' WHERE Notice = '0'");
-                    $add_row->execute();
-                } catch (Exception $e) {
-                    $ok = false;
-                    echo $e . PHP_EOL;
-                    exit;
-                }
-                if ($ok) echo "<br/>Modified!<br/>";
-            }
-            fclose($file);
-            echo "<br /> OK";
-        } else echo "Error opening file";
+        $ok = true;
+        try {
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $add_row = $pdo->prepare("UPDATE $match_code SET Notice = '' WHERE Notice = '0'");
+            $add_row->execute();
+        } catch (Exception $e) {
+            $ok = false;
+            echo $e . PHP_EOL;
+            exit;
+        }
+        if ($ok) echo "<br/>Modified!<br/>";
     }
 ?>
